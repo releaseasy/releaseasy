b88c603ec23980e7b207e3fb4481cd66dd2248f4
ajiho <lujiahao@88.com>
2026-08-29 21:24:41 +0800

    从cac迁移到commander
    Signed-off-by: ajiho <lujiahao@88.com>

--- src/cli.js
+++ src/cli.js
@@ -1,71 +1,84 @@
 #!/usr/bin/env node
 
-import { cac } from "cac";
-import lt from "semver/functions/lt.js";
+import { Command } from "commander";
 import pkg from "../package.json" with { type: "json" };
 import { CancelledError, ExitSignal } from "./errors.js";
 import { logger } from "./utils/index.js";
 import { resolveConfig } from "./config/index.js";
-import { resolve } from "node:path";
 import CONSTANTS from "./constants/index.js";
 
-const cli = cac(CONSTANTS.CLI_NAME);
-cli.help().version(pkg.version, "-V, --version");
+const program = new Command();
 
-cli
-  .command("[release]", "Release Package")
-  .alias("release")
+program
+  .name(CONSTANTS.CLI_NAME)
+  .version(pkg.version, "-V, --version")
+  .helpOption("-h, --help")
+  .helpCommand(false);
+
+/**
+ * release
+ *
+ * releaseasy
+ * releaseasy release
+ */
+const releaseCommand = new Command("release")
+  .description("Release Package")
   .option("-C, --cwd <path>", "Run the release process in the specified directory")
-  .option("-d, --dry-run", "Simulate release without applying changes.", { default: false })
+  .option("-d, --dry-run", "Simulate release without applying changes.", false)
   .option("-c, --config <path>", "Path to the config file")
-  .option("-v, --verbose", "Increases the logging verbosity", {
-    type: [],
-  })
-  .action(async (_, inlineOptions) => {
-    const { release } = await import("./release.js");
+  .option("-v, --verbose", "Increases the logging verbosity");
 
-    const options = await resolveConfig(inlineOptions);
+releaseCommand.action(async (options) => {
+  const { release } = await import("./release.js");
 
-    await release(options);
+  const resolvedOptions = await resolveConfig(options);
+
+  await release(resolvedOptions);
+});
+
+program.addCommand(releaseCommand, {
+  isDefault: true,
+});
+
+/**
+ * changelog
+ *
+ * releaseasy changelog [git-cliff args...]
+ */
+const changelogCommand = new Command("changelog")
+  .description("Run git-cliff")
+  .helpOption(false)
+  .allowUnknownOption(true)
+  .allowExcessArguments(true);
+
+changelogCommand.action(async (_, command) => {
+  const { runGitCliff } = await import("./git-cliff.js");
+
+  const result = await runGitCliff(command.args, {
+    throwOnError: false,
   });
 
-cli
-  .command("changelog [...args]", "Run git-cliff")
-  .allowUnknownOptions()
-  .option("--git-cliff-help", "Show git-cliff help")
-  .action(async (_, options) => {
-    const { runGitCliff } = await import("./git-cliff.js");
+  process.exit(result.exitCode ?? 0);
+});
 
-    if (options.gitCliffHelp) {
-      const result = await runGitCliff(["--help"], {
-        throwOnError: false,
-      });
-
-      process.exit(result.exitCode ?? 0);
-    }
-    const args = cli.rawArgs.slice(3);
-
-    const result = await runGitCliff(args, {
-      throwOnError: false,
-    });
-    process.exit(result.exitCode ?? 0);
-  });
+program.addCommand(changelogCommand);
 
 try {
-  cli.parse(process.argv, { run: false });
-  await cli.runMatchedCommand();
+  await program.parseAsync(process.argv);
 } catch (err) {
-  if (!err) process.exit(0);
+  if (!err) {
+    process.exit(0);
+  }
 
   if (err instanceof CancelledError) {
     logger.warn(err.message);
   } else if (err instanceof ExitSignal) {
     process.exit(err.code);
   } else if (err instanceof Error) {
-    console.log("-------------------");
     logger.error(err);
   } else {
-    logger.error(`Unknown error`, err);
+    logger.error("Unknown error", err);
   }
+
   process.exit(1);
 }
