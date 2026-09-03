4f1225d302da9c682d0903ba11008698654ac1a0
ajiho <lujiahao@88.com>
2026-09-04 00:42:40 +0800

    字体加粗
    Signed-off-by: ajiho <lujiahao@88.com>

--- src/init.js
+++ src/init.js
@@ -1,3 +1,28 @@
-export async function init(options) {
-  console.log(options);
+import { select, confirm } from "@inquirer/prompts";
+import ansis from "ansis";
+
+export async function init(inlineOps) {
+  const options = await promptInit();
+}
+
+async function promptInit() {
+  console.log(`\n${ansis.green.bold("Welcome to releaseasy!")}\n`);
+
+  const configFormat = await select({
+    message: "Which config format would you like?",
+    choices: [
+      {
+        name: "JavaScript",
+        value: "javascript",
+      },
+      {
+        name: "TypeScript",
+        value: "typescript",
+      },
+      {
+        name: "JSON",
+        value: "json",
+      },
+    ],
+  });
 }
