eddebe424c8f97c4ec0bb4957c43921f78f54ae6
ajiho <lujiahao@88.com>
2026-09-11 15:08:13 +0800

    使用合适的检测文件大小的包
    Signed-off-by: ajiho <lujiahao@88.com>

--- .size-limit.mjs
+++ .size-limit.mjs
@@ -1,10 +1,10 @@
 export default [
   {
-    limit: "0.6KB",
+    limit: "0.1KB",
     path: "./dist/index.js",
   },
   {
-    limit: "0.6KB",
+    limit: "5KB",
     path: "./dist/cli.js",
   },
 ];
