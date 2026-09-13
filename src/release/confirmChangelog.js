0029d17e6fb32e0bae4b7a28eb52f9bf224c7459
ajiho <lujiahao@88.com>
2026-09-14 01:20:11 +0800

    确认变更日志也应该判断日志功能是否被启用
    Signed-off-by: ajiho <lujiahao@88.com>

--- src/release/confirmChangelog.js
+++ src/release/confirmChangelog.js
@@ -2,6 +2,7 @@ import { confirm } from "@inquirer/prompts";
 import { cancel } from "../handleError.js";
 
 export async function confirmChangelog(options, context) {
+  if (options.git.changelog === false) return;
   const normal = await confirm({
     message: "Changelog generated. Does it look good?",
     default: true,
